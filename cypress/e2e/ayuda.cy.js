3f506347f98731a896936712837b9c02adf32bca
Javier <javmoli045@gmail.com>
2023-06-26 21:33:56 -0600

    Agregado test de ayuda

---
+++ cypress/e2e/ayuda.cy.js
@@ -0,0 +1,20 @@
+const url = "https://ride-customer-dev.web.app/";
+
+beforeEach(() => {
+  cy.visit(url + "login");
+});
+
+it('Iniciar sesión', () => {
+  cy.get('input[name="email"]').type('pollolandiasolutions@gmail.com');
+  cy.get('input[name="password"]').type('pollo12345');
+  
+  cy.get('form').submit();
+  
+  cy.wait(700);
+
+  cy.get('ion-menu-button').click()
+  cy.contains('Ayuda').click()
+
+  cy.wait(300);
+  cy.url().should('eq', url + "contact");
+});
